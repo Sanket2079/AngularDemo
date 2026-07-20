53809f17065919c235860511b57f6210c9469863
Sanket Dhaygude <sanketdhaigude7517@gmail.com>
2026-07-20 11:59:55 +0530

    Credit Comp Added

---
+++ src/app/Component/Credit/a-credit-comp/a-credit-comp.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-a-credit-comp',
+  imports: [],
+  templateUrl: './a-credit-comp.html',
+  styleUrl: './a-credit-comp.scss',
+})
+export class ACreditComp {}
